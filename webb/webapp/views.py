b7fdf22af0d898e1a49615165f42b0ff14d57da2
Rabi Basukala <ibarbasukala77@gmail.com>
2023-08-22 20:17:58 +0545

    webapp start

--- webb/webapp/views.py
+++ webb/webapp/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def home(request):
+    if request.method == "POST":
+        age = float(request.POST["age"])
+
+        return render(request, "result.html", {})
+    return render(request, "home.html", {})
